a466b42c68c6bfb63483d92fd3635ff96a69958c
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2020-05-22 23:20:08 +0300

    Gotovoya_2
    попытка 2. ГОТОВАЯ прога

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@
 #include "teoria.h"
 #define PI 3.14159265
 #define G 9.8
+#define E 2.7182
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -41,28 +42,19 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::printGraphic(int speed, int angle, double distance, double height, double Fsopr,double mas){
+void MainWindow::printGraphic(int speed, int angle, double distance, double height, double coff,double mas){
     QVector<double> x(1000), y(1000);
-
-    double Fsopr_x=Fsopr*cos(angle*PI/180); //силы сопротивления для х и у
-    double Fsopr_y=Fsopr*sin(angle*PI/180);
-    double max_time = 2 * speed * sin(angle * PI / 180) / G;   //max время
 
     double maxAxis = 0,
            value = 0,
-            step=1/1000;
-           //step =distance / 1000;
+           step =distance / 1000;
 
-    //x[0]=0;
     double time = step;
     for (int i=0;i < 1000;i++)
     {
-      //x[i] = value;
-      //y[i] = x[i] * tan(angle * PI / 180) - (G / (2 * pow(speed, 2) * pow(cos(angle * PI / 180), 2))) * pow(x[i], 2);
-        x[i]=speed*cos(angle*PI/180)*time+Fsopr_x*time*time/(2*mas);
-        y[i]=speed*sin(angle*PI/180)*time-(G-Fsopr_y)*time*time/(2*mas);
-       //value+=step;
-        ui->coffec->setNum(x[i]); //вывод силы сопротивления
+        x[i]=speed*cos(angle*PI/180)*(mas/coff)*(1-pow(E,(-coff*time/mas)));
+        y[i]=(mas/coff)*((speed*sin(angle*PI/180)+mas*G/coff)*(1-pow(E,(-coff*time/mas)))-G*time);
+
       time += step;
     }
 
@@ -87,17 +79,15 @@ void MainWindow::on_buttonStart_clicked() //кнопка вычислить
 
     double coff = 0.5*0.15*PI*radius*radius*plotn; //рассчитываем коэффициент сопротивления k=0.5*C*S*ro C=0.15 S=pi*r^2 ro=1.29
     double Fsopr = coff*speed*speed; //вычисление силы сопротивления F=k*V^2
-    //ui->coffec->setNum(Fsopr); //вывод силы сопротивления
 
     double time = 2 * speed * sin(angle * PI / 180) / G;   //время
     double distance = pow(speed, 2) * sin(2 * angle * PI / 180) / G;  //дистанция
     double height = pow(speed * sin(angle * PI / 180), 2) / (2 * G);   //высота
 
     ui->resultTime->setNum(time);   //вывод результатов
-    ui->resultDistance->setNum(distance);
     ui->resultHeight->setNum(height);
 
-    printGraphic(speed, angle, distance, height, Fsopr,mas);
+    printGraphic(speed, angle, distance, height, coff,mas);
 }
 
 void MainWindow::on_action_triggered()
